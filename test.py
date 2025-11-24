087bfea4ca3db5d043e94cae15d11c55932ec5ee
mNiloy695 <mniloy695@gmail.com>
2025-11-25 02:43:21 +0600

    test gemini api for generate image

--- test.py
+++ test.py
@@ -59,17 +59,50 @@ from google.genai import types
 #     print("Could not parse JSON because status is not 200.")
 
 #see all model of gemini by api call
-import requests
+# import requests
 
-API_KEY = ""
-url = f"https://generativelanguage.googleapis.com/v1/models?key={API_KEY}"
+# API_KEY = ""
+# url = f"https://generativelanguage.googleapis.com/v1/models?key={API_KEY}"
 
-response = requests.get(url)
+# response = requests.get(url)
 
-print("STATUS:", response.status_code)
-print("RAW:", response.text)
+# print("STATUS:", response.status_code)
+# print("RAW:", response.text)
 
-if response.status_code == 200:
-    print("JSON:", response.json())
-else:
-    print("Could not parse JSON because status is not 200.")
+# if response.status_code == 200:
+#     print("JSON:", response.json())
+# else:
+#     print("Could not parse JSON because status is not 200.")
+
+
+# fla ai api
+# "74eb235b-2f39-426e-a526-26a2a52b1695:e430c6166fd9fe24479512fe9f6d8d87"
+
+
+
+import fal_client
+import os
+os.environ["FAL_KEY"] = "74eb235b-2f39-426e-a526-26a2a52b1695:e430c6166fd9fe24479512fe9f6d8d87"
+def on_queue_update(update):
+    if isinstance(update, fal_client.InProgress):
+        for log in update.logs:
+           print(log["message"])
+
+try:
+    result = fal_client.subscribe(
+    "fal-ai/flux/dev",
+    arguments={
+        "prompt": "a cat",
+        "seed": 6252023,
+        "image_size": "landscape_4_3",
+        "num_images": 4
+    },
+    with_logs=True,
+    on_queue_update=on_queue_update,
+    headers={"X-Custom-Header": "value"},  # Optional: custom headers
+    )
+    print(result)
+except Exception as e:
+    print("Error during image generation:", e)
+
+
